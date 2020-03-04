1771e956348f451645a5c8047d879af1163bd0d0
cdliudefu <cdliudefu@126.com>
2020-03-04 09:39:03 +0800

    Create About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1 class="title">this is an about page</h1>
+  </div>
+</template>
+
+<style lang="less">
+  .title{
+    color: blue;
+  }
+</style>
+
+<script>
+export default {
+  data(){
+    return {}
+  },
+  mounted(){
+    console.log('100'.includes('1'))
+  }
+}
+</script>
