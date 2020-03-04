1f80ed6a4e1a46bebe8ea0a9704e147f1aa1a1d8
cdliudefu <cdliudefu@126.com>
2020-03-04 09:38:47 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png" alt="">
+    <HelloWorld msg="Welcome to your vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+  name:'Home',
+  components:{
+    HelloWorld
+  },
+  mounted(){
+    console.log('home')
+  }
+}
+</script>
